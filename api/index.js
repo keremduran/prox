0d041a2e13b809e5c02cdea9b4546c2cd0e28f2b
Kerem Duran <kerem@weareoutpost.ca>
2023-10-31 19:14:15 -0400

    body parser added

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const app = require('express')();
 const { v4 } = require('uuid');
 var postmark = require("postmark");
 let nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
@@ -17,57 +20,6 @@ app.get('/api/item/:slug', (req, res) => {
 });
 
 app.post('/api/email', (req, res) => { 
-	console.log("TEST");
-
-	let transporter;
-	
-	try {
-		transporter = nodemailer.createTransport({
-			port: 465,
-			host: 'smtp.gmail.com',
-			auth: {
-				user: 'unsecuretestprojects@gmail.com',
-				pass: "wjrc pwyx fkxh bvvk",
-			},
-			secure: true,
-		});
-	} catch (error) {
-		console.log(error);
-	}
-
-	if(transporter) {
-		console.log("TYPE OF TRANSPORTER: ",typeof transporter);
-		console.log("TRANSPORTER KEYS: ", JSON.stringify(Object.keys(transporter)));
-	}
-
-
-	const mailData = {
-    from: 'unsecuretestprojects@gmail.com',
-    to: 'keremduran.fw@gmail.com',
-    subject: `Message From`,
-    html: `<div>Test</div><p>Sent from:test</p>`,
-  };
-
-	try {
-
-		console.log("MAIL SENDING!");
-
-		transporter.sendMail(mailData, function (error, info) {
-			if (error) {
-				console.log(error);
-				throw new Error(error);
-			} else {
-				console.log("Email Sent", info);
-				return true;
-			}
-		});
-	} catch (error) {
-		console.log(error)
-	}
-
-
-
-
 	// const mailData = {
   //   from: 'inspiredigital.test@gmail.com',
   //   to: 'keremduran.fw@gmail.com',
@@ -92,8 +44,10 @@ app.post('/api/email', (req, res) => {
 	// 	res.end(error);
 	// }
 
-	console.log(JSON.stringify(Object.keys(req)));
-	res.end("Test")
+  console.log(req.body);
+
+  // send back a response with the same data
+  res.json(req.body);
 });
 
 
