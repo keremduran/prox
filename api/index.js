7b42ef06cd9863417439e4de96155aba423bb424
Kerem Duran <kerem@weareoutpost.ca>
2023-10-31 12:23:11 -0400

    testing email

--- api/index.js
+++ api/index.js
@@ -16,9 +16,22 @@ app.get('/api/item/:slug', (req, res) => {
 });
 
 app.post('/api/email', (req, res) => {  
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'text/html');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.end({test: "test"});
+
+	// Send an email:
+	var client = new postmark.ServerClient("3dbab09c-f2a1-44e5-8fa2-14c3a9a66b33");
+
+	client.sendEmail({
+			"From": "kerem@weareoutpost.ca",
+			"To": "kerem@weareoutpost.ca",
+			"Subject": "Hello from Postmark",
+			"HtmlBody": "<strong>Hello</strong> dear Postmark user.",
+			"TextBody": "Hello from Postmark!",
+			"MessageStream": "outbound"
+	});
+
+  res.end("Test");
 });
 
 
